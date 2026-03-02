2783e8338fbd5cbbcb402354bab6b58af945e3b1
koustubh <koustubhrayamane2001@gmail.com>
2026-03-02 16:39:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+ //Task -1
+
+// let name = "Koustubh";
+// let age = 25;
+// let city = "Bangalore";
+// console.log("My name is " + name + ".");
+// console.log("I am " + age + " years old.");
+// console.log("I am from " + city + ".");
+
+ // Task -2
+// let userName = prompt("Enter your name:"); 
+// alert("Welcome, " + userName + "!"); 
+// let choice = confirm("Do you want to learn JavaScript?");
+// if (choice) {
+//     alert("Great Choice!");
+// } else {
+//     alert("No Problem!");
+// }
+
+// Task -3 
+// let myString = "Hello World!";
+// let myNumber = 500;
+// let myBoolean = true;
+// let myUndefined;
+// let myNull = null; 
+// console.log("Value:", myString, "| Type:", typeof myString);
+// console.log("Value:", myNumber, "| Type:", typeof myNumber);
+// console.log("Value:", myBoolean, "| Type:", typeof myBoolean);
+// console.log("Value:", myUndefined, "| Type:", typeof myUndefined);
+// console.log("Value:", myNull, "| Type:", typeof myNull); 
+
+// Task -4 
+// let colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple"]; 
+// console.log("First color:", colors[0]); 
+// console.log("Last color:", colors[colors.length - 1]); 
+// console.log("Total number of colors:", colors.length); 
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("Color at index", i + ":", colors[i]);
+// }
+
+// Task -5 
+// let subjects = ["Math", "Science", "English", "History", "Computer"]; 
+// let index = prompt("Enter a number between 0 and 4:"); 
+// index = Number(index); 
+// if (index >= 0 && index < subjects.length) {
+//     console.log("Selected Subject:", subjects[index]);
+// } else {
+//     console.log("Invalid index! Please enter a number between 0 and 4.");
+// }
+
+// Task -6 
+// let student = {
+//     name: "Koustubh",
+//     age: 25,
+//     course: "MECH",
+//     skills: ["HTML", "CSS", "JavaScript"]
+// };
+// console.log("Student Name:", student.name);
+// console.log("Second Skill:", student.skills[1]);
+// console.log("Course Name:", student.course);
+
+// Task -7 
+// let product = {
+//     name: "Laptop",
+//     price: 45000,
+//     category: "Electronics",
+//     inStock: true
+// }; 
+// console.log("Product Name:", product.name);
+// console.log("Price:", product.price); 
+// if (product.inStock) {
+//     console.log("Available : Yes");
+// } else {
+//     console.log("Out of Stock");
+// }
+
+// Task -8 
+// let fruits = {
+//     tropical: ["mango", "pineapple"],
+//     berries: ["strawberry", "blueberry"],
+//     citrus: ["orange", "lemon"]
+// }; 
+// console.log("Blueberry:", fruits.berries[1]);
+// console.log("Mango:", fruits.tropical[0]);
+// console.log("Lemon:", fruits.citrus[1]); 
+// console.log("Total Categories:", Object.keys(fruits).length);
